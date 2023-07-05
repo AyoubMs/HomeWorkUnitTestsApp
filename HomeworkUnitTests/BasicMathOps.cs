a801224d77cff3c58e700917987146528ee2c864
AyoubMS <chahayb563@gmail.com>
2023-07-05 13:06:59 +0200

    add app

---
+++ HomeworkUnitTests/BasicMathOps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeworkUnitTests
+{
+    public class BasicMathOps
+    {
+        public int CalculateInts(int a, int b, string operation)
+        {
+            switch (operation)
+            {
+                case "*":
+                    return a * b;
+                case "/":
+                    return a / b;
+                case "-":
+                    return a - b;
+                case "+":
+                    return a + b;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
